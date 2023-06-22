025ee2becbdb9bb56010149b9687a067dda442e2
MathieuH <56202917+MatHuguet@users.noreply.github.com>
2023-06-22 22:07:10 +0200

    add styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import Header from './Header'
+import Card from './Card'
+import data from '../data'
 import '../styles/app.css'
 
-
 export default function App() {
+    const card = data.map(item => {
+        return (
+            <Card 
+                key={item.id}
+                {...item}
+            />
+        )
+    })
+    
+
     return (
         <div className='container'>
             <Header/>
+            {card}
         </div>
     )
 } 
