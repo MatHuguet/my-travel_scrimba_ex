936af70e7d9d3d55ed0a75514f87c13ffd00ff57
MathieuH <huguet.mathieu@gmail.com>
2023-06-23 11:52:57 +0200

    fixed cards cont size. styling ok

--- src/components/Card.js
+++ src/components/Card.js
@@ -21,14 +21,15 @@ export default function Card(props) {
                     
                     <a className="google-link" href={props.linkGooglemap}>View on Google Maps</a>
                 </div>
+                <div className="card-title">
+                    <h3 className="title">{props.title}</h3>
+                </div>
                 <div className="travel-dates">
                     <p>{props.travelDate}</p>
+                    <p> - </p>
                     <p>{props.backDate}</p>
                 </div>
-                <div className="card-text">
-                    <h3 className="title">{props.title}</h3>
                     <p className="card-impression">{props.impressionText}</p>
-                </div>
 
 
             </div>
--- src/data.js
+++ src/data.js
@@ -6,8 +6,8 @@ export default [
         title: "Des paysages somptueux",
         imgName: "andrea-zanenga-YGGv1hdokNk-unsplash.jpg",
         linkGooglemap: "https://www.google.com/maps/place/Mad%C3%A8re,+Portugal/@32.693435,-17.2003722,10z/data=!4m6!3m5!1s0xc5fe9a0f8f92eef:0xc77e56774ba5e6f!8m2!3d32.7607074!4d-16.9594723!16zL20vMDRfejE?entry=ttu",
-        travelDate: "2 Jul 2017",
-        backDate: "13 Jul 2017",
+        travelDate: "2 Jul, 2017",
+        backDate: "13 Jul, 2017",
         impressionText: "Madère, c’est ce bout de terre portugaise à peine visible sur une carte, isolé dans l’Atlantique, au large de la côte nord-ouest de l’Afrique. Surnommée “l’île aux fleurs” ou “jardin de l’Atlantique”, le territoire possède de nombreux atouts : une température généralement comprise entre 15°C et 20°C."
     },
     {
@@ -15,9 +15,9 @@ export default [
         country: "SARDAIGNE",
         title: "Cagliari",
         imgName: "chloe-frost-smith-CAEKQsss1bQ-unsplash.jpg",
-        linkGooglemap: "https://www.google.com/maps/place/Ville+m%C3%A9tropolitaine+de+Cagliari,+Italie/@39.179002,8.8331875,9.75z/data=!4m6!3m5!1s0x12e74c6bf0607ccf:0x308be15cb0e8320!8m2!3d39.2237632!4d9.1218671!16zL20vMDUzMTRz?entry=ttu",
-        travelDate: "3 Aug 2022",
-        backDate: "16 Aug 2022",
+        linkGooglemap: "https://www.google.com/maps/@39.2184119,9.1189861,11.17z?entry=ttu",
+        travelDate: "3 Aug, 2022",
+        backDate: "16 Aug, 2022",
         impressionText: "Cagliari est la capitale de la Sardaigne, une île italienne. Elle est célèbre pour le quartier médiéval du Castello, entouré de remparts et situé au sommet de la colline, bien au-dessus du reste de la ville. Parmi les monuments importants, on trouve la cathédrale de Cagliari, datant du XIIIe siècle."
 
     },
@@ -27,8 +27,28 @@ export default [
         title: "El Teide",
         imgName: "hert-niks-S7QZcYLJJsA-unsplash.jpg",
         linkGooglemap: "https://www.google.com/maps/place/Tenerife/@28.2810409,-17.1481727,9z/data=!4m6!3m5!1s0xc4029effe8682ed:0xb01a4bf1c84baf3c!8m2!3d28.2915637!4d-16.6291304!16zL20vMDFucWIx?entry=ttu",
-        travelDate: "23 Jan 2020",
-        backDate: "02 Fev 2020",
+        travelDate: "23 Jan, 2020",
+        backDate: "02 Fev, 2020",
+        impressionText: "Le Teide est la troisième structure volcanique la plus haute et volumineuse de la planète, derrière le Mauna loa et Mauna kea à Hawaï, et le plus haut sommet des îles Canaries et de toute l'Espagne."
+    },
+    {
+        id: 2,
+        country: "TENERIFE",
+        title: "El Teide",
+        imgName: "hert-niks-S7QZcYLJJsA-unsplash.jpg",
+        linkGooglemap: "https://www.google.com/maps/place/Tenerife/@28.2810409,-17.1481727,9z/data=!4m6!3m5!1s0xc4029effe8682ed:0xb01a4bf1c84baf3c!8m2!3d28.2915637!4d-16.6291304!16zL20vMDFucWIx?entry=ttu",
+        travelDate: "23 Jan, 2020",
+        backDate: "02 Fev, 2020",
+        impressionText: "Le Teide est la troisième structure volcanique la plus haute et volumineuse de la planète, derrière le Mauna loa et Mauna kea à Hawaï, et le plus haut sommet des îles Canaries et de toute l'Espagne."
+    },
+    {
+        id: 2,
+        country: "TENERIFE",
+        title: "El Teide",
+        imgName: "hert-niks-S7QZcYLJJsA-unsplash.jpg",
+        linkGooglemap: "https://www.google.com/maps/place/Tenerife/@28.2810409,-17.1481727,9z/data=!4m6!3m5!1s0xc4029effe8682ed:0xb01a4bf1c84baf3c!8m2!3d28.2915637!4d-16.6291304!16zL20vMDFucWIx?entry=ttu",
+        travelDate: "23 Jan, 2020",
+        backDate: "02 Fev, 2020",
         impressionText: "Le Teide est la troisième structure volcanique la plus haute et volumineuse de la planète, derrière le Mauna loa et Mauna kea à Hawaï, et le plus haut sommet des îles Canaries et de toute l'Espagne."
     }
     
